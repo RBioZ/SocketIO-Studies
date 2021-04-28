63f7c8d37f83273f6fbc546989fada440f334335
Jonatha Rihan <jonatha.rihan@clicksoft.com.br>
2021-04-27 21:14:11 -0300

    :sparkles: Chat!

--- Client/src/client.ts
+++ Client/src/client.ts
@@ -1,7 +1,29 @@
-import io, { Socket } from "socket.io-client";
+import io from "socket.io-client";
+import readline from "readline";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const socket = io("http://localhost:3000");
 
 socket.on("connect", async () => {
-  console.log(socket.id);
+  //console.log("[MY ID]: " + socket.id);
 });
+
+socket.on("message", (msg: string) => {
+  console.log(msg);
+});
+
+var waitForUserInput = () => {
+  rl.question(">", (msg) => {
+    if (msg == "/quit") {
+      rl.close();
+    } else {
+      socket.emit("message", msg);
+      waitForUserInput();
+    }
+  });
+};
+
+waitForUserInput();
--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,14 +1,24 @@
-import axios from "axios";
-import express, { Request, Response } from "express";
-import ioserver, { Socket } from "socket.io";
-import ioclient from "socket.io-client";
+import express from "express";
+import { Socket } from "socket.io";
 const app = express();
 const server = require("http").Server(app);
 const port = 3000;
 let io = require("socket.io")(server);
 
+interface IMessage {
+  content: string;
+}
+
 io.on("connection", (socket: Socket) => {
-  console.log(socket.id);
+  console.log("\x1b[32m%s\x1b[0m", "[NEW CONNECTION]: ", socket.id);
+
+  socket.on("disconnect", function () {
+    console.log("\x1b[31m%s\x1b[0m", "[DISCONECTED]: ", socket.id);
+  });
+
+  socket.on("message", (msg: string) => {
+    socket.broadcast.emit("message", `${msg}`);
+  });
 });
 
 server.listen(port, () =>
